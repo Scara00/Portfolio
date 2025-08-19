67eee4e1555458b3efea0437f0a0b7fe724dfbec
ScarattiCristian <cristian.scaratti@alten.it>
2025-08-19 18:57:44 +0200

    fixed path

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,7 @@ import Other from "./pages/Other";
 import GamingSetup3D from "./components/GamingSetup3D";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/other" element={<Other />} />
-        <Route path="/3d-setup" element={<GamingSetup3D />} />
-        <Route path="/" element={<Portfolio />} />
-      </Routes>
-    </Router>
-  );
+  return <Portfolio />;
 }
 
 export default App;
