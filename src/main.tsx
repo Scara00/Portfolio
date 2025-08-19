70900e0f2d4b576d3aa4339b5c49e703c0df0772
ScarattiCristian <cristian.scaratti@alten.it>
2025-08-19 18:51:19 +0200

    added insights pt.2

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <SpeedInsights></SpeedInsights>
+    <Analytics />
     <App />
   </StrictMode>
 );
