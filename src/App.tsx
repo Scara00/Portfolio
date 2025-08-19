d3e3559f154da6e11521d1e6bc944f88fd2b3337
ScarattiCristian <cristian.scaratti@alten.it>
2025-08-19 18:59:13 +0200

    removed unusued imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Portfolio from "./pages/portfolio";
-import Other from "./pages/Other";
-import GamingSetup3D from "./components/GamingSetup3D";
 
 function App() {
   return <Portfolio />;
