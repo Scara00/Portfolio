9a5857c2eb8edf3dc0e5e61df353e7bf91d0d6b0
ScarattiCristian <cristian.scaratti@alten.it>
2025-08-19 18:56:18 +0200

    fixed error build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./pages/portfolio";
+import Other from "./pages/Other";
+import GamingSetup3D from "./components/GamingSetup3D";
 
 function App() {
-  return <Portfolio />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/other" element={<Other />} />
+        <Route path="/3d-setup" element={<GamingSetup3D />} />
+        <Route path="/" element={<Portfolio />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
